fe02d79ed5b10eea7ee0124151ccaa7d1cf97a08
German Blejman <gblejman@gmail.com>
2022-07-06 11:35:25 -0300

    fix intercom config key

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   env: process.env.NODE_ENV,
   intercom: {
-    token: process.env.INTERCOM_ACCESS_TOKEN ?? "",
+    token: process.env.INTERCOM_TOKEN ?? "",
     adminId: process.env.INTERCOM_ADMIN_ID ?? "",
     outboundMessage: process.env.INTERCOM_OUTBOUND_MESSAGE ?? "",
   },
