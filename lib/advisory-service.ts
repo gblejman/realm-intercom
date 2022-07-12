1fe68f0f26f703baca1efc025e411f7c6ace0394
German Blejman <gblejman@gmail.com>
2022-07-12 11:44:13 -0300

    simplify setup

--- lib/advisory-service.ts
+++ lib/advisory-service.ts
@@ -75,68 +75,18 @@ export const createAdvisory = ({ realm }: { realm: RealmService }) => {
       }
     }
 
-    // Find best possible existing contact matching user fields:
-    // Users's id/email/full_name vs contact's external_id/email/name
-    // Must disregard nulls (would actually match contacts with null fields)
-    if (!contact) {
+    // No contact, find by email if exists
+    if (!contact && user.email) {
       const searchContactRes = await intercom.contacts.search({
         data: {
           query: {
-            operator: Operators.OR,
-            value: [
-              {
-                operator: Operators.AND,
-                value: [
-                  {
-                    field: "external_id",
-                    operator: Operators.EQUALS,
-                    value: String(user.id),
-                  },
-                  {
-                    field: "external_id",
-                    operator: Operators.NOT_EQUALS,
-                    value: null,
-                  },
-                ],
-              },
-              {
-                operator: Operators.AND,
-                value: [
-                  {
-                    field: "email",
-                    operator: Operators.EQUALS,
-                    value: user.email,
-                  },
-                  {
-                    field: "email",
-                    operator: Operators.NOT_EQUALS,
-                    value: null,
-                  },
-                ],
-              },
-              {
-                operator: Operators.AND,
-                value: [
-                  {
-                    field: "name",
-                    operator: Operators.EQUALS,
-                    value: user.full_name,
-                  },
-                  {
-                    field: "name",
-                    operator: Operators.NOT_EQUALS,
-                    value: null,
-                  },
-                ],
-              },
-            ],
+            field: "email",
+            operator: Operators.EQUALS,
+            value: user.email,
           },
         },
       });
 
-      // If multiple matches - ie: one by email, other by name, the correct action would probably be merging them
-      // absolutely ensuring they belong to the same entity.
-      // Better to consider multi match as not found and create a new one
       if (searchContactRes.total_count == 1) {
         contact = searchContactRes.data[0];
       }
@@ -153,16 +103,13 @@ export const createAdvisory = ({ realm }: { realm: RealmService }) => {
 
     console.log("upsertPayload", upsertPayload);
 
-    // Got contact but missing link for any reason, ie: added via intercom with email, never linked
-    if (contact && !contact.external_id) {
+    // Upsert
+    if (contact) {
       contact = await intercom.contacts.update({
         id: contact.id,
         ...upsertPayload,
       });
-    }
-
-    // Still no contact, create a new linked one
-    if (!contact) {
+    } else {
       contact = await intercom.contacts.createUser(upsertPayload);
     }
 
@@ -185,34 +132,46 @@ export const createAdvisory = ({ realm }: { realm: RealmService }) => {
 
     console.log("message", message);
 
-    // Could be all set if message.conversation_id was returned when createConversationWithoutContactReply: true
-    // but not working... TODO: open ticket
+    let conversationId;
 
-    // Workaround: Find the conversation which source.id matches the outbound message id
-    // Must give Intercom a few seconds to index conversation, won't find it if not, weird...
-    // https://github.com/intercom/intercom-node/pull/319/files/f02265bcc865f8791bd131b5a5bcf55218e79deb
-    await delay(5000);
+    if (message.conversation_id) {
+      conversationId = message.conversation_id;
+    } else {
+      // Could be all set if message.conversation_id was returned when createConversationWithoutContactReply: true
+      // but not working... TODO: open ticket
 
-    const searchConversationRes = await intercom.conversations.search({
-      data: {
-        query: {
-          field: "source.id",
-          operator: Operators.EQUALS,
-          value: message.id,
+      // Workaround: Find the conversation which source.id matches the outbound message id
+      // Must give Intercom a few seconds to index conversation, won't find it if not, weird...
+      // https://github.com/intercom/intercom-node/pull/319/files/f02265bcc865f8791bd131b5a5bcf55218e79deb
+      await delay(5000);
+
+      const searchConversationRes = await intercom.conversations.search({
+        data: {
+          query: {
+            field: "source.id",
+            operator: Operators.EQUALS,
+            value: message.id,
+          },
         },
-      },
-    });
+      });
 
-    const conversation = searchConversationRes.conversations[0];
+      const conversation = searchConversationRes.conversations[0];
 
-    console.log("CONVERSATION", conversation);
+      conversationId = conversation.id;
+    }
 
     const updatedUser = await realm.users.update({
       id: "me",
       social_facebook: JSON.stringify({
         intercom_contact_id: contact.id,
-        intercom_conversation_id: conversation.id,
+        intercom_conversation_id: conversationId,
       }),
+    });
+
+    console.log("setup", {
+      contactId: contact.id,
+      messageId: message.id,
+      conversationId,
     });
 
     return updatedUser;
